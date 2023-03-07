29d49434d01519bf67a8a9da3a1af83af9ee20e3
Arif Hossain <hossaininfo@ymail.com>
2023-03-07 13:38:36 +0100

    Initial commit

---
+++ src/main/java/no/lagalt/lagaltbackend/LagaltBackendApplication.java
@@ -0,0 +1,13 @@
+package no.lagalt.lagaltbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LagaltBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LagaltBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/lagalt/lagaltbackend/LagaltBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package no.lagalt.lagaltbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LagaltBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
