e88d323754c79ba4e7a8a93c07d1153f86fb156f
Arif Hossain <hossaininfo@ymail.com>
2023-03-09 16:40:04 +0100

    End points in i ProjectController

--- src/main/java/no/lagalt/lagaltbackend/controller/ProjectController.java
+++ src/main/java/no/lagalt/lagaltbackend/controller/ProjectController.java
@@ -1,13 +1,66 @@
 package no.lagalt.lagaltbackend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import no.lagalt.lagaltbackend.pojo.dto.ProjectDto;
+import no.lagalt.lagaltbackend.pojo.dto.ProjectMapper;
+import no.lagalt.lagaltbackend.pojo.entity.Project;
 import no.lagalt.lagaltbackend.service.ProjectService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/project")
 @RequiredArgsConstructor
 public class ProjectController {
     private final ProjectService projectService;
+    private final ProjectMapper projectMapper;
+
+    @Operation(summary = "GET ALL PROJECT")
+    @GetMapping
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<ProjectDto> getAllProject() {
+        Collection<Project> projects = projectService.findAll();
+        return projects.stream()
+                .map(projectMapper::toProjectDto)
+                .collect(Collectors.toList());
+
+    }
+
+    @Operation(summary = "GET SINGLE PROJECT")
+    @GetMapping("/{projectId}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public ProjectDto getProjectById(@PathVariable("projectId") int projectId) {
+        return projectMapper.toProjectDto(projectService.findById(projectId));
+    }
+
+    @Operation(summary = "CREATE PROJECT")
+    @GetMapping("/create")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public ProjectDto create(@RequestBody ProjectDto projectDto) {
+        Project project = projectMapper.toProject(projectDto);
+        return projectMapper.toProjectDto(projectService.create(project));
+    }
+
+
+    @Operation(summary = "UPDATE SINGLE PROJECT")
+    @GetMapping("/update/{projectId}")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public ProjectDto updateProjectById(@RequestBody ProjectDto projectDto, @PathVariable("projectId") int projectId) {
+        Project project = projectMapper.toProject(projectDto);
+        return projectMapper.toProjectDto(projectService.update(projectId, project));
+    }
+
+    @Operation(summary = "DELETE SINGLE PROJECT")
+    @GetMapping("/delete/{projectId}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public String deleteProjectById(@PathVariable("projectId") int projectId) {
+        projectService.deleteById(projectId);
+        return "PROJECT_DELETED";
+    }
+
 }
