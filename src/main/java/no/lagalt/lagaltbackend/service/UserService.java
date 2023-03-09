44c0cb4d3a654a5cbe3fb9e9625ad2794b3967be
Arif Hossain <hossaininfo@ymail.com>
2023-03-09 13:50:13 +0100

    added class diagram and database diagram on project root

--- src/main/java/no/lagalt/lagaltbackend/service/UserService.java
+++ src/main/java/no/lagalt/lagaltbackend/service/UserService.java
@@ -2,7 +2,5 @@ package no.lagalt.lagaltbackend.service;
 
 import no.lagalt.lagaltbackend.pojo.entity.AppUser;
 
-import java.util.List;
-
 public interface UserService extends CrudService<AppUser,Integer>{
 }
