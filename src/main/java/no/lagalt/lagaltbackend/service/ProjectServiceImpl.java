5c6c8d94819c17c441809172ebddc78a0b03fbfb
Zakhida Ruslanova <zakhida_sul@hotmail.se>
2023-03-10 07:55:52 +0100

    added implementation of CRUD methods in projectServiceImpl

--- src/main/java/no/lagalt/lagaltbackend/service/ProjectServiceImpl.java
+++ src/main/java/no/lagalt/lagaltbackend/service/ProjectServiceImpl.java
@@ -1,6 +1,7 @@
 package no.lagalt.lagaltbackend.service;
 
 import lombok.RequiredArgsConstructor;
+import no.lagalt.lagaltbackend.exception.ResourceNotFoundException;
 import no.lagalt.lagaltbackend.pojo.entity.Project;
 import no.lagalt.lagaltbackend.repository.ProjectRepository;
 import org.springframework.stereotype.Service;
@@ -13,26 +14,39 @@ public class ProjectServiceImpl implements ProjectService{
     private final ProjectRepository projectRepository;
     @Override
     public Collection<Project> findAll() {
-        return null;
+        return projectRepository.findAll();
     }
 
     @Override
-    public Project findById(Integer integer) {
-        return null;
+    public Project findById(Integer projectId) {
+        return projectRepository.findById(projectId).get();
     }
 
     @Override
-    public Project create(Project entity) {
-        return null;
+    public Project create(Project project) {
+        return projectRepository.save(project);
     }
 
     @Override
-    public Project update(Integer integer, Project entity) {
-        return null;
+    public Project update(Integer projectId, Project project) {
+        Project foundProject = getProjectById(projectId);
+        foundProject.setTitle(project.getTitle());
+        foundProject.setDescription(project.getDescription());
+        foundProject.setTheme(project.getTheme());
+        foundProject.setProject_status(project.getProject_status());
+        foundProject.setProject_type(project.getProject_type());
+        foundProject.setParticipants(project.getParticipants());
+        foundProject.setSkills(project.getSkills());
+        return projectRepository.save(foundProject);
     }
+
 
     @Override
     public void deleteById(Integer integer) {
 
+    }
+    private Project getProjectById(Integer integer) {
+        return projectRepository.findById(integer)
+                .orElseThrow(() -> new ResourceNotFoundException("PROJECT_NOT_EXIST"));
     }
 }
