284fa50dd97ecd6b0d7aceffe7c730eb872e294f
Arif Hossain <hossaininfo@ymail.com>
2023-03-11 19:26:25 +0100

    removed costruktors and added anotation in JwtResponse

--- src/main/java/no/lagalt/lagaltbackend/security/JwtResponse.java
+++ src/main/java/no/lagalt/lagaltbackend/security/JwtResponse.java
@@ -1,24 +1,20 @@
 package no.lagalt.lagaltbackend.security;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import no.lagalt.lagaltbackend.pojo.dto.AppUserDto;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
 public class JwtResponse implements Serializable {
 
 
+    @Serial
     private static final long serialVersionUID = -8091879091924046844L;
     private final String jwttoken;
     private AppUserDto user;
 
-    public JwtResponse(String jwttoken, AppUserDto user) {
-        this.jwttoken = jwttoken;
-        this.user = user;
-    }
-
-    public String getToken() {
-        return this.jwttoken;
-    }
 }
