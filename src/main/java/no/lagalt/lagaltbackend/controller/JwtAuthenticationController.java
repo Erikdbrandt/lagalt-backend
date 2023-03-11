f1d77ff8e991c30f60a65a3acdc706f9d785e2ae
Arif Hossain <hossaininfo@ymail.com>
2023-03-11 19:09:26 +0100

    create token return AppUserDto

--- src/main/java/no/lagalt/lagaltbackend/controller/JwtAuthenticationController.java
+++ src/main/java/no/lagalt/lagaltbackend/controller/JwtAuthenticationController.java
@@ -2,6 +2,8 @@ package no.lagalt.lagaltbackend.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import no.lagalt.lagaltbackend.pojo.dto.AppUserDto;
+import no.lagalt.lagaltbackend.pojo.dto.AppUserMapper;
 import no.lagalt.lagaltbackend.pojo.entity.AppUser;
 import no.lagalt.lagaltbackend.security.JwtRequest;
 import no.lagalt.lagaltbackend.security.JwtResponse;
@@ -28,6 +30,7 @@ public class JwtAuthenticationController {
     private final JwtTokenUtil jwtTokenUtil;
     private final JwtUserDetailsService userDetailsService;
     private final UserService userService;
+    private final AppUserMapper userMapper;
 
 
 
@@ -45,17 +48,16 @@ public class JwtAuthenticationController {
 
         authenticate(authenticationRequest.getEmail(), authenticationRequest.getPassword());
 
-
         final UserDetails userDetails = userDetailsService
                 .loadUserByUsername(authenticationRequest.getEmail());
 
         final String token = jwtTokenUtil.generateToken(userDetails);
 
         AppUser user = userService.getByUsername(authenticationRequest.getEmail());
+        AppUserDto appUserDto = userMapper.toAppUserDto(user);
+        JwtResponse jwtResponse = new JwtResponse(token, appUserDto);
 
-        JwtResponse resp = new JwtResponse(token, user);
-
-        return ResponseEntity.ok(resp);
+        return ResponseEntity.ok(jwtResponse);
     }
 
     private void authenticate(String username, String password) throws Exception {
--- src/main/java/no/lagalt/lagaltbackend/security/JwtResponse.java
+++ src/main/java/no/lagalt/lagaltbackend/security/JwtResponse.java
@@ -1,7 +1,7 @@
 package no.lagalt.lagaltbackend.security;
 
 import lombok.Data;
-import no.lagalt.lagaltbackend.pojo.entity.AppUser;
+import no.lagalt.lagaltbackend.pojo.dto.AppUserDto;
 
 import java.io.Serializable;
 
@@ -11,9 +11,9 @@ public class JwtResponse implements Serializable {
 
     private static final long serialVersionUID = -8091879091924046844L;
     private final String jwttoken;
-    private AppUser user;
+    private AppUserDto user;
 
-    public JwtResponse(String jwttoken, AppUser user) {
+    public JwtResponse(String jwttoken, AppUserDto user) {
         this.jwttoken = jwttoken;
         this.user = user;
     }
